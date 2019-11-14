44b0280680b5c625fc4631a0b1f5c99b1e756239
Gustavo Mora Gonzalez <gmora008@gmail.com>
2019-11-13 18:08:30 -0600

    fill the username automatically

--- lib/basic_chat_web/live/index.ex
+++ lib/basic_chat_web/live/index.ex
@@ -2,7 +2,7 @@ defmodule BasicChatWeb.Live.Index do
   use Phoenix.LiveView
 
   alias BasicChat.Chat
-  alias BasicChat.Chat.Message
+  # alias BasicChat.Chat.Message
   alias BasicChatWeb.ChatView
 
   require Logger
@@ -21,16 +21,19 @@ defmodule BasicChatWeb.Live.Index do
     |> Enum.into(%{"datetime" => DateTime.utc_now()})
     |> Chat.create_message()
 
-    {:noreply, fetch(socket)}
+    {:noreply, fetch(socket, params["username"])}
   end
 
   def handle_info({Chat, [:message, _event_type], _message}, socket) do
-    {:noreply, fetch(socket)}
+    {:noreply, fetch(socket, get_username(socket))}
   end
 
-  defp fetch(socket) do
-    changeset = Chat.change_message(%Message{})
+  defp fetch(socket, username \\ nil) do
     messages = Chat.list_messages()
-    assign(socket, messages: messages, changeset: changeset)
+    assign(socket, messages: messages, username: username)
+  end
+
+  defp get_username(socket) do
+    socket.assigns.username
   end
 end
